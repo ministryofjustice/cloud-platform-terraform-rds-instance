e97653c00dce2b7a177e1fe3f5aaa38a040d7215
Nayo Akinyele <nayoa@hotmail.co.uk>
2018-08-23 15:55:10 +0100

    fix module

--- example/output.tf
+++ example/output.tf
@@ -1,46 +1,46 @@
 output "access_key_id" {
     description = "Access key id for RDS account"
-    value       = "${aws_iam_access_key.user.id}"
+    value       = "${module.example_team_rds.access_key_id}"
 }
 
 output "secret_access_key" {
     description = "Secret access key for RDS account"
-    value       = "${aws_iam_access_key.user.secret}"
+    value       = "${module.example_team_rds.secret_access_key}"
 }
 output "rds_instance_endpoint" {
     description = "The connection endpoint in address:port format"
-    value       = "${aws_db_instance.rds.endpoint}"
+    value       = "${module.example_team_rds.rds_instance_endpoint}"
 } 
 
 output "rds_instance_arn" {
     description = "The ARN of the RDS instance"
-    value       = "${aws_db_instance.rds.arn}"
+    value       = "${module.example_team_rds.rds_instance_arn}"
 }
 output "database_name" {
     description = "Name of the database"
-    value       = "${aws_db_instance.rds.name}"
+    value       = "${module.example_team_rds.database_name}"
 }
 
 output "database_username" {
     description = "Database Username"
-    value       = "${aws_db_instance.rds.username}"
+    value       = "${module.example_team_rds.database_username}"
 }
 
 output "database_password" {
     description = "Database Password"
-    value       = "${aws_db_instance.rds.password}"
+    value       = "${module.example_team_rds.database_password}"
 }
 
 output "database_subnet_group_arn" {
     description = "The ARN of the db subnet group."
-    value       = "${aws_db_subnet_group.db_subnet.name}"
+    value       = "${module.example_team_rds.database_subnet_group_arn}"
 }
 output "kms_key_arn" {
     description = "The Amazon Resource Name ARN of the key."
-    value       = "${aws_kms.kms_key.arn}"
+    value       = "${module.example_team_rds.kms_key_arn}"
 }
 
 output "kms_key_id" {
     description = "The globally unique identifier for the KMS key."
-    value       = "${aws_kms_key.kms.key_id}"
+    value       = "${module.example_team_rds.kms_key_id}"
 }
