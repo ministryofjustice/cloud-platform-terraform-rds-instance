d725cb164c9be99ad6dab315137b8af8ed1722f6
Nayo Akinyele <nayoa@hotmail.co.uk>
2018-09-03 17:47:03 +0100

    Terraform format tf files

--- example/main.tf
+++ example/main.tf
@@ -1,24 +1,23 @@
-  provider "aws" {
+provider "aws" {
   region = "eu-west-1"
 }
-  
-  module "example_team_rds" {
-  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance"
-  
-  team_name                   = "example-repo"
-  db_allocated_storage        = "100"
-  db_engine                   = "mysql"
-  db_engine_version           = "5.7.17"
-  db_instance_class           = "db.t2.small"
-  db_backup_retention_period  = 10
-  db_storage_type             = "io1"
-  db_iops                     = "1000"
-  db_vpc_security_group_ids   = ["sg-7e8cf203", "sg-7e8cf203"]
-  db_db_subnet_groups         = ["subnet-7293103a", "subnet-7bf10c21", "subnet-de00b3b8"]
-  business-unit               = "example-bu"
-  application                 = "exampleapp"
-  is-production               = "false"
-  environment-name            = "development"
-  infrastructure-support      = "example-team@digtal.justice.gov.uk"
 
-  }
+module "example_team_rds" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance"
+
+  team_name                  = "example-repo"
+  db_allocated_storage       = "100"
+  db_engine                  = "mysql"
+  db_engine_version          = "5.7.17"
+  db_instance_class          = "db.t2.small"
+  db_backup_retention_period = 10
+  db_storage_type            = "io1"
+  db_iops                    = "1000"
+  db_vpc_security_group_ids  = ["sg-7e8cf203", "sg-7e8cf203"]
+  db_db_subnet_groups        = ["subnet-7293103a", "subnet-7bf10c21", "subnet-de00b3b8"]
+  business-unit              = "example-bu"
+  application                = "exampleapp"
+  is-production              = "false"
+  environment-name           = "development"
+  infrastructure-support     = "example-team@digtal.justice.gov.uk"
+}
--- output.tf
+++ output.tf
@@ -1,52 +1,54 @@
 output "access_key_id" {
-    description = "Access key id for RDS account"
-    value       = "${aws_iam_access_key.user.id}"
+  description = "Access key id for RDS account"
+  value       = "${aws_iam_access_key.user.id}"
 }
 
 output "secret_access_key" {
-    description = "Secret access key for RDS account"
-    value       = "${aws_iam_access_key.user.secret}"
+  description = "Secret access key for RDS account"
+  value       = "${aws_iam_access_key.user.secret}"
 }
+
 output "rds_instance_endpoint" {
-    description = "The connection endpoint in address:port format"
-    value       = "${aws_db_instance.rds.endpoint}"
-} 
+  description = "The connection endpoint in address:port format"
+  value       = "${aws_db_instance.rds.endpoint}"
+}
 
 output "rds_instance_id" {
-    description = "The RDS instance ID."
-    value       = "${aws_db_instance.rds.id}"
+  description = "The RDS instance ID."
+  value       = "${aws_db_instance.rds.id}"
 }
 
 output "rds_instance_arn" {
-    description = "The ARN of the RDS instance"
-    value       = "${aws_db_instance.rds.arn}"
+  description = "The ARN of the RDS instance"
+  value       = "${aws_db_instance.rds.arn}"
 }
+
 output "database_name" {
-    description = "Name of the database"
-    value       = "${aws_db_instance.rds.name}"
+  description = "Name of the database"
+  value       = "${aws_db_instance.rds.name}"
 }
 
 output "database_username" {
-    description = "Database Username"
-    value       = "${aws_db_instance.rds.username}"
+  description = "Database Username"
+  value       = "${aws_db_instance.rds.username}"
 }
 
 output "database_password" {
-    description = "Database Password"
-    value       = "${aws_db_instance.rds.password}"
+  description = "Database Password"
+  value       = "${aws_db_instance.rds.password}"
 }
 
 output "database_subnet_group_arn" {
-    description = "The ARN of the db subnet group."
-    value       = "${aws_db_subnet_group.db_subnet.name}"
+  description = "The ARN of the db subnet group."
+  value       = "${aws_db_subnet_group.db_subnet.name}"
 }
+
 output "kms_key_arn" {
-    description = "The Amazon Resource Name ARN of the key."
-    value       = "${aws_kms_key.kms.arn}"
+  description = "The Amazon Resource Name ARN of the key."
+  value       = "${aws_kms_key.kms.arn}"
 }
 
 output "kms_key_id" {
-    description = "The globally unique identifier for the KMS key."
-    value       = "${aws_kms_key.kms.key_id}"
+  description = "The globally unique identifier for the KMS key."
+  value       = "${aws_kms_key.kms.key_id}"
 }
-
