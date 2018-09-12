cddaaf773572491fd8eead9d746ee631704de2b7
Dimitrios Karagiannis <dhkarag@gmail.com>
2018-09-12 13:28:36 +0100

    Update example to reflect changes.

--- example/main.tf
+++ example/main.tf
@@ -5,16 +5,23 @@ provider "aws" {
 module "example_team_rds" {
   source = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance"
 
+  /*
+   * When using this module through the cloud-platform-environments, the
+   * following two values are automatically supplied by the pipeline as
+   * variables.
+   *
+   */
+  // cluster_name           = "cloud-platform-live-0"
+  // cluster_state_bucket   = "cloud-platform-cluster-state-bucket"
+
   team_name                  = "example-repo"
   db_allocated_storage       = "100"
-  db_engine                  = "mysql"
-  db_engine_version          = "5.7.17"
+  db_engine                  = "postgres"
+  db_engine_version          = "10.4"
   db_instance_class          = "db.t2.small"
   db_backup_retention_period = 10
   db_storage_type            = "io1"
   db_iops                    = "1000"
-  db_vpc_security_group_ids  = ["sg-7e8cf203", "sg-7e8cf203"]
-  db_db_subnet_groups        = ["subnet-7293103a", "subnet-7bf10c21", "subnet-de00b3b8"]
   business-unit              = "example-bu"
   application                = "exampleapp"
   is-production              = "false"
