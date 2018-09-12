ac5d5e7259465ea3d8130bc941b206ca3c03dfbb
Dimitrios Karagiannis <dhkarag@gmail.com>
2018-09-12 12:21:31 +0100

    Remove unneeded outputs.
    These are all outputs that the user does not need to know about in order to consume the service and that they cannot
    use even if we do export them as outputs.

--- example/output.tf
+++ example/output.tf
@@ -1,21 +1,6 @@
-output "access_key_id" {
-  description = "Access key id for RDS account"
-  value       = "${module.example_team_rds.access_key_id}"
-}
-
-output "secret_access_key" {
-  description = "Secret access key for RDS account"
-  value       = "${module.example_team_rds.secret_access_key}"
-}
-
 output "rds_instance_endpoint" {
   description = "The connection endpoint in address:port format"
   value       = "${module.example_team_rds.rds_instance_endpoint}"
-}
-
-output "rds_instance_arn" {
-  description = "The ARN of the RDS instance"
-  value       = "${module.example_team_rds.rds_instance_arn}"
 }
 
 output "database_name" {
@@ -31,19 +16,4 @@ output "database_username" {
 output "database_password" {
   description = "Database Password"
   value       = "${module.example_team_rds.database_password}"
-}
-
-output "database_subnet_group_arn" {
-  description = "The ARN of the db subnet group."
-  value       = "${module.example_team_rds.database_subnet_group_arn}"
-}
-
-output "kms_key_arn" {
-  description = "The Amazon Resource Name ARN of the key."
-  value       = "${module.example_team_rds.kms_key_arn}"
-}
-
-output "kms_key_id" {
-  description = "The globally unique identifier for the KMS key."
-  value       = "${module.example_team_rds.kms_key_id}"
 }
