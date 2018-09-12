2c1ccb796e3ccb3c6b07887e36be1f68320558f7
Dimitrios Karagiannis <dhkarag@gmail.com>
2018-09-12 13:31:53 +0100

    Use internal subnets for the RDS security group.
    We only need to allow traffic between cluster nodes and the instance, which will both live in the internal subnets.

--- main.tf
+++ main.tf
@@ -66,18 +66,22 @@ resource "aws_security_group" "rds-sg" {
   description = "Allow all inbound traffic"
   vpc_id      = "${data.terraform_remote_state.cluster.vpc_id}"
 
+  // We cannot use `${aws_db_instance.rds.port}` here because it creates a
+  // cyclic dependency. Rather than resorting to `aws_security_group_rule` which
+  // is not ideal for managing rules, we will simply allow traffic to all ports.
+  // This does not compromise security as the instance only listens on one port.
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["${data.terraform_remote_state.cluster.network_cidr_block}"]
+    cidr_blocks = ["${data.terraform_remote_state.cluster.internal_subnets}"]
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks = ["${data.terraform_remote_state.cluster.network_cidr_block}"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["${data.terraform_remote_state.cluster.internal_subnets}"]
   }
 }
 
