f3777625c9c6f049fbe7af882d18df350955de92
Dimitrios Karagiannis <dhkarag@gmail.com>
2018-10-04 15:08:17 +0100

    Reorganise variables and update README
    All variables are now strings.

--- variables.tf
+++ variables.tf
@@ -1,8 +1,38 @@
+variable "cluster_name" {
+  description = "The name of the cluster (eg.: cloud-platform-live-0)"
+}
+
+variable "cluster_state_bucket" {
+  description = "The name of the S3 bucket holding the terraform state for the cluster"
+}
+
 variable "team_name" {}
+
+variable "application" {}
+
+variable "environment-name" {}
+
+variable "is-production" {
+  default = "false"
+}
+
+variable "business-unit" {
+  description = "Area of the MOJ responsible for the service"
+  default     = "mojdigital"
+}
+
+variable "infrastructure-support" {
+  description = "The team responsible for managing the infrastructure. Should be of the form <team-name> (<team-email>)"
+}
+
+variable "snapshot_identifier" {
+  description = "Specifies whether or not to create this database from a snapshot. This correlates to the snapshot ID you'd find in the RDS console."
+  default     = ""
+}
 
 variable "db_allocated_storage" {
   description = "The allocated storage in gibibytes"
-  default     = 10
+  default     = "10"
 }
 
 variable "db_engine" {
@@ -12,7 +42,7 @@ variable "db_engine" {
 
 variable "db_engine_version" {
   description = "The engine version to use e.g. 10.4"
-  default     = 10.4
+  default     = "10.4"
 }
 
 variable "db_instance_class" {
@@ -22,44 +52,15 @@ variable "db_instance_class" {
 
 variable "db_backup_retention_period" {
   description = "The days to retain backups. Must be 1 or greater to be a source for a Read Replica"
+  default     = "7"
 }
 
 variable "db_iops" {
   description = "The amount of provisioned IOPS. Setting this to a value other than 0 implies a storage_type of io1"
-  default     = 0
+  default     = "0"
 }
 
 variable "db_name" {
   description = "The name of the database to be created on the instance (if empty, it will be the generated random identifier)"
-  default     = ""
-}
-
-variable "business-unit" {
-  description = " Area of the MOJ responsible for the service"
-  default     = "mojdigital"
-}
-
-variable "application" {}
-
-variable "is-production" {
-  default = "false"
-}
-
-variable "environment-name" {}
-
-variable "infrastructure-support" {
-  description = "The team responsible for managing the infrastructure. Should be of the form <team-name> (<team-email>)"
-}
-
-variable "cluster_name" {
-  description = "The name of the cluster (eg.: cloud-platform-live-0)"
-}
-
-variable "cluster_state_bucket" {
-  description = "The name of the S3 bucket holding the terraform state for the cluster"
-}
-
-variable "snapshot_identifier" {
-  description = "Specifies whether or not to create this database from a snapshot. This correlates to the snapshot ID you'd find in the RDS console."
   default     = ""
 }
