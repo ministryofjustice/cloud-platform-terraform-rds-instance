e47918bca9fe2ebf003ef74109dcb0b4394d8db9
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-05-05 12:57:46 +0100

    try mock

--- test/unit-test/main.tf
+++ test/unit-test/main.tf
@@ -1,7 +1,17 @@
 provider "aws" {
-  access_key = "mock_access_key"
-  secret_key = "mock_secret_key"
-  region     = "eu-west-2"
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  region                      = "eu-west-2"
+  s3_force_path_style         = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    ec2            = "http://localhost:5000"
+    iam            = "http://localhost:5001"
+    rds            = "http://localhost:5002"
+  }
 }
 
 module "rds" {
