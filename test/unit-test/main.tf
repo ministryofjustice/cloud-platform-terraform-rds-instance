608a84bff3f071ee4aa7499ed6bd5d3132363785
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-05-05 09:42:50 +0100

    start work on test

---
+++ test/unit-test/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  region                      = "eu-west-2"
+  s3_force_path_style         = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    apigateway     = "http://localhost:4566"
+    cloudformation = "http://localhost:4566"
+    cloudwatch     = "http://localhost:4566"
+    dynamodb       = "http://localhost:4566"
+    ec2            = "http://localhost:4566"
+    es             = "http://localhost:4566"
+    elasticache    = "http://localhost:4566"
+    firehose       = "http://localhost:4566"
+    iam            = "http://localhost:4566"
+    kinesis        = "http://localhost:4566"
+    lambda         = "http://localhost:4566"
+    rds            = "http://localhost:4566"
+    redshift       = "http://localhost:4566"
+    route53        = "http://localhost:4566"
+    s3             = "http://localhost:4566"
+    secretsmanager = "http://localhost:4566"
+    ses            = "http://localhost:4566"
+    sns            = "http://localhost:4566"
+    sqs            = "http://localhost:4566"
+    ssm            = "http://localhost:4566"
+    stepfunctions  = "http://localhost:4566"
+    sts            = "http://localhost:4566"
+  }
+}
+
+module "rds" {
+  source = "../../"
+
+  cluster_name = "rds.cloud-platform.service.justice.gov.uk"
+  team_name     = "example-repo"
+  business-unit = "example-bu"
+  application   = "example-app"
+  is-production = "false"
+  namespace     = "example-ns"
+}
--- test/unit-test/versions.tf
+++ test/unit-test/versions.tf
@@ -1,12 +1,11 @@
-
 terraform {
-  required_version = ">= 0.14"
   required_providers {
     aws = {
       source = "hashicorp/aws"
     }
-    random = {
-      source = "hashicorp/random"
+    kubernetes = {
+      source = "hashicorp/kubernetes"
     }
   }
+  required_version = ">= 0.14"
 }
