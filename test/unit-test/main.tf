64507eabb42175747fd83cb2a957a18fa226dcd6
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-07-26 12:56:52 +0100

    try another IP range

--- test/unit-test/main.tf
+++ test/unit-test/main.tf
@@ -21,10 +21,10 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
 
   name                   = local.vpc_name
-  cidr                   = "10.0.0.0/8"
+  cidr                   = "10.0.0.0/16"
   azs                    = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
-  private_subnets        = ["10.1.0.0/16", "10.2.0.0/16", "10.3.0.0/16"]
-  public_subnets         = ["10.4.0.0/16", "10.5.0.0/16", "10.6.0.0/16"]
+  private_subnets        = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets         = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_nat_gateway     = false
   enable_vpn_gateway     = false
   create_egress_only_igw = false
