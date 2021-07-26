ec7483a4e2328495b73b09f3c24a9c7a891d699a
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-07-26 12:11:31 +0100

    pin providers

--- test/unit-test/versions.tf
+++ test/unit-test/versions.tf
@@ -1,10 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
+      version = "1.11.0"
     }
   }
   required_version = ">= 0.14"
