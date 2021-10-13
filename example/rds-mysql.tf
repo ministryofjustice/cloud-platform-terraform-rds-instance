090f26f76ea6f9aa65f690cd6f33b937aabaafe8
Razvan Cosma <75040720+razvan-moj@users.noreply.github.com>
2021-10-13 14:23:00 +0100

    Fix cli tf resources creation. (#112)

--- example/rds-mysql.tf
+++ example/rds-mysql.tf
@@ -1,27 +1,20 @@
 /*
- * When using this module through the cloud-platform-environments,
- * this variable is automatically supplied by the pipeline.
- *
-*/
-
-variable "cluster_name" {}
-
-/*
  * Make sure that you use the latest version of the module by changing the
  * `ref=` value in the `source` attribute to the latest version listed on the
  * releases page of this repository.
  *
 */
 
-module "rds" {
-  source        = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.5"
-  team_name              = "example-repo"
-  business-unit          = "example-bu"
-  application            = "example-app"
-  is-production          = "false"
-  namespace              = "example-ns"
-  environment-name       = "example-env"
-  infrastructure-support = "example-team"
+module "rds_mysql" {
+  source                 = "github.com/ministryofjustice/cloud-platform-terraform-rds-instance?ref=5.16.5"
+  cluster_name           = var.cluster_name
+  team_name              = var.team_name
+  business-unit          = var.business_unit
+  application            = var.application
+  is-production          = var.is_production
+  environment-name       = var.environment_name
+  infrastructure-support = var.infrastructure_support
+  namespace              = var.namespace
 
   # turn off performance insights
   performance_insights_enabled = false
@@ -53,32 +46,32 @@ module "rds" {
   }
 }
 
-resource "kubernetes_secret" "rds" {
+resource "kubernetes_secret" "rds_mysql" {
   metadata {
-    name      = "rds-instance-output"
+    name      = "rds-mysql-instance-output"
     namespace = var.namespace
   }
 
   data = {
-    rds_instance_endpoint = module.rds.rds_instance_endpoint
-    database_name         = module.rds.database_name
-    database_username     = module.rds.database_username
-    database_password     = module.rds.database_password
-    rds_instance_address  = module.rds.rds_instance_address
-    access_key_id         = module.rds.access_key_id
-    secret_access_key     = module.rds.secret_access_key
+    rds_instance_endpoint = module.rds_mysql.rds_instance_endpoint
+    database_name         = module.rds_mysql.database_name
+    database_username     = module.rds_mysql.database_username
+    database_password     = module.rds_mysql.database_password
+    rds_instance_address  = module.rds_mysql.rds_instance_address
+    access_key_id         = module.rds_mysql.access_key_id
+    secret_access_key     = module.rds_mysql.secret_access_key
   }
 }
 
-resource "kubernetes_config_map" "rds" {
+resource "kubernetes_config_map" "rds_mysql" {
   metadata {
-    name      = "rds-instance-output"
+    name      = "rds-mysql-instance-output"
     namespace = var.namespace
   }
 
   data = {
-    database_name = module.rds.database_name
-    db_identifier = module.rds.db_identifier
+    database_name = module.rds_mysql.database_name
+    db_identifier = module.rds_mysql.db_identifier
 
   }
 }
