0c0361931df7eeb36de6fdf9e8f907464448fbf1
jakemulley <jakemulley@users.noreply.github.com>
2022-08-16 14:53:13 +0100

    Add variable types and descriptions, use # for comments, add provider versions

--- variables.tf
+++ variables.tf
@@ -1,53 +1,75 @@
 variable "cluster_name" {
   description = "The name of the cluster (eg.: cloud-platform-live-0)"
+  type        = string
 }
 
-variable "team_name" {}
+variable "team_name" {
+  type        = string
+  description = "Team name"
+}
 
-variable "application" {}
+variable "application" {
+  type        = string
+  description = "Application name"
+}
 
-variable "environment-name" {}
+variable "environment-name" {
+  type        = string
+  description = "Environment name"
+}
 
 variable "is-production" {
-  default = "false"
+  default     = "false"
+  description = "Whether the environment is production or not"
+  type        = string
 }
 
 variable "namespace" {
-  default = ""
+  default     = ""
+  description = "Namespace name"
+  type        = string
 }
 
 variable "business-unit" {
   description = "Area of the MOJ responsible for the service"
   default     = ""
+  type        = string
 }
 
 variable "infrastructure-support" {
   description = "The team responsible for managing the infrastructure. Should be of the form <team-name> (<team-email>)"
+  type        = string
 }
 
 variable "rds_name" {
   description = "Optional name of the RDS cluster. Changing the name will re-create the RDS"
   default     = ""
+  type        = string
 }
 
 variable "snapshot_identifier" {
   description = "Specifies whether or not to create this database from a snapshot. This correlates to the snapshot ID you'd find in the RDS console."
   default     = ""
+  type        = string
 }
 
 variable "db_allocated_storage" {
   description = "The allocated storage in gibibytes"
   default     = "10"
+  type        = string
 }
 
 variable "db_max_allocated_storage" {
   description = "Maximum storage limit for storage autoscaling"
   default     = "10000"
+  type        = string
 }
 
 variable "db_engine" {
   description = "Database engine used e.g. postgres, mysql, sqlserver-ex"
   default     = "postgres"
+  type        = string
+
   validation {
     condition     = contains(["postgres", "mysql", "mariadb", "sqlserver-ee", "sqlserver-ex", "sqlserver-se", "sqlserver-web"], var.db_engine)
     error_message = "Choose one of Postgresql, MySQL or Microsoft SQL Server. For Aurora, see https://github.com/ministryofjustice/cloud-platform-terraform-rds-aurora."
@@ -57,16 +79,19 @@ variable "db_engine" {
 variable "db_engine_version" {
   description = "The engine version to use e.g. 13.2 for Postgresql, 8.0 for MySQL, 15.00.4073.23.v1 for MS-SQL. Omitting the minor release part allows for automatic updates."
   default     = "10"
+  type        = string
 }
 
 variable "db_instance_class" {
   description = "The instance type of the RDS instance"
   default     = "db.t2.small"
+  type        = string
 }
 
 variable "db_backup_retention_period" {
   description = "The days to retain backups. Must be 1 or greater to be a source for a Read Replica"
   default     = "7"
+  type        = string
 }
 
 variable "db_iops" {
@@ -78,26 +103,31 @@ variable "db_iops" {
 variable "db_name" {
   description = "The name of the database to be created on the instance (if empty, it will be the generated random identifier)"
   default     = ""
+  type        = string
 }
 
 variable "allow_minor_version_upgrade" {
   description = "Indicates that minor version upgrades are allowed."
   default     = "true"
+  type        = string
 }
 
 variable "allow_major_version_upgrade" {
   description = "Indicates that major version upgrades are allowed."
   default     = "false"
+  type        = string
 }
 
 variable "rds_family" {
   description = "Maps the engine version with the parameter group family, a family often covers several versions"
   default     = "postgres10"
+  type        = string
 }
 
 variable "ca_cert_identifier" {
   description = "Specifies the identifier of the CA certificate for the DB instance"
   default     = "rds-ca-2019"
+  type        = string
 }
 
 variable "performance_insights_enabled" {
--- versions.tf
+++ versions.tf
@@ -1,12 +1,13 @@
-
 terraform {
   required_version = ">= 0.14"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = ">=3.0.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
+      version = ">=2.0.0"
     }
   }
 }
