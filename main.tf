e0fab8f258875b1ab0bcc7208903f885ddaf6c03
tom-j-smith <thomas.smith@digital.justice.gov.uk>
2024-02-21 16:21:01 +0000

    Change resourse permissions

--- main.tf
+++ main.tf
@@ -266,8 +266,7 @@ data "aws_iam_policy_document" "irsa" {
 
     resources = [
       local.db_arn,
-      "arn:aws:rds:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:deployment:bgd-*",
-      "arn:aws:rds:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:db:*"
+      "arn:aws:rds:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:deployment:bgd-*"
     ]
   }
 }
