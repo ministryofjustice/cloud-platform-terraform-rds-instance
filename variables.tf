545b6b26482fdbb4091a58d942101b8812677325
Tim Cheung <152907271+timckt@users.noreply.github.com>
2024-07-10 12:54:29 +0100

    feat: Upgrade storage class to gp3 and io2

--- variables.tf
+++ variables.tf
@@ -60,7 +60,7 @@ variable "db_backup_retention_period" {
 }
 
 variable "db_iops" {
-  description = "The amount of provisioned IOPS. Setting this to a value other than 0 implies a storage_type of io1"
+  description = "The amount of provisioned IOPS. Setting this to a value other than 0 implies a storage_type of io2"
   default     = 0
   type        = number
 }
